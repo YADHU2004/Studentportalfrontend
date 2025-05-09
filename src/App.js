8e4994136c53a26ff5b37850ab59d4c5716374bb
YADHU2004 <yadhuanand2004@gmail.com>
2025-05-09 21:21:54 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { Layout } from "antd";
-import { Route, Routes } from "react-router-dom";
+
 import { CopyrightOutlined } from "@ant-design/icons";
 
 import "./App.css";
@@ -9,7 +9,7 @@ import StudentHome from "./components/Student/Home/StudentHome";
 import WrappedNormalLoginForm from "./components/Login/Login";
 import AttendancePage from "./components/Student/AttendancePage/AttendancePage";
 import Header from "./components/UI Components/Header/Header";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Classes from "./index.module.css";
 import Error404 from "./components/Error404/Error404";
 import AssignmentHome from "./components/Student/Assignment/AssignmentHome";
