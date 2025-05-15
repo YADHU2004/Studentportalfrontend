0fb7608e70f22eb2665d471c86b212776aea49fe
YADHU2004 <yadhuanand2004@gmail.com>
2025-05-15 15:44:32 +0530

    upload

--- src/components/UI Components/Header/Header.js
+++ src/components/UI Components/Header/Header.js
@@ -1,4 +1,4 @@
-/*import React, { Component } from "react";
+import React, { Component } from "react";
 import { Layout, Typography } from "antd";
 import { Link } from "react-router-dom"; 
 import Classes from "./Header.module.css";
@@ -17,38 +17,6 @@ class Header extends Component {
   }
 }
 
-export default Header;*/
-import React, { Component } from "react";
-import { Layout, Typography } from "antd";
-import { Link } from "react-router-dom";
-import { UserOutlined } from "@ant-design/icons";
-import Classes from "./Header.module.css";
-
-class Header extends Component {
-  render() {
-    const username = localStorage.getItem("username") || "User";
-    const userType = localStorage.getItem("userType") || "student";
-    const title =
-      userType === "admin"
-        ? "Elite Scholar's Admin Portal"
-        : "Elite Scholar's Student Portal";
-
-    return (
-      <Layout.Header className={Classes.header}>
-        <Link to={userType === "admin" ? "/admin" : "/StudentHome"} className={Classes.link}>
-          <Typography.Title level={1} className={Classes.title}>
-            {title}
-          </Typography.Title>
-        </Link>
-        <div className={Classes.userSection}>
-          <UserOutlined style={{ marginRight: 8, color: "#fff" }} />
-          <Typography.Text style={{ color: "#fff" }}>
-            Hi, {username}
-          </Typography.Text>
-        </div>
-      </Layout.Header>
-    );
-  }
-}
-
 export default Header;
+
+ 
